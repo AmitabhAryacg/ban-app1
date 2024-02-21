aa23c04449063d236fcf96680aa9953091ce3da8
amitabharya-CG <amitabh.arya@capgemini.com>
2024-02-21 17:31:50 -0600

    First commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        const body = figlet.textSync("Microservice 1 written in Bun!");
+        return new Response(body);
+    },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
