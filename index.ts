77438618b52b661e8cfedcaab03a2a9414342525
AmitabhAryacg <149522446+AmitabhAryacg@users.noreply.github.com>
2024-03-13 19:08:24 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import figlet from 'figlet';
 
 const server = Bun.serve({
-    port: 80,
+    port: 3000,
     fetch(req) {
         const body = figlet.textSync("Microservice 1 written in Bun!");
         return new Response(body);
